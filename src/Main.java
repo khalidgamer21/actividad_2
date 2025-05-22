151fb207ba8e39b3a7da964a822fb4009dae8ea2
marlon <marlon.monterrosa.m@gmail.com>
2025-05-21 20:16:49 -0500

    Se capturan los datos de las 5 personas.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         GestionPersonas gestion = new GestionPersonas();
+        ArrayList<Usuario> personas = gestion.capturarUsuario(scanner);
 
 
         for (int i = 1; i <= 5; i++) {
