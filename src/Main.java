ec519ef3fbfe7d7495dfe6cc42da9010f33b1d89
marlon <marlon.monterrosa.m@gmail.com>
2025-05-21 20:18:20 -0500

    Se muestran los nombres y géneros.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         GestionPersonas gestion = new GestionPersonas();
-        ArrayList<Usuario> personas = gestion.capturarUsuario(scanner);
+        ArrayList<Usuario> usuarios = gestion.capturarUsuario(scanner);
 
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("\n--- Datos Capturados ---");
+        gestion.mostrar_Nombre_Y_Genero(usuarios);
+
     }
-}
