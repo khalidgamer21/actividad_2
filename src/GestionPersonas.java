7d5b8e0fe1bc72a861b8a4c2308da789639f316c
marlon <marlon.monterrosa.m@gmail.com>
2025-05-21 19:57:23 -0500

    Un bucle for repite 5 veces para pedir datos de 5 personas.

--- src/GestionPersonas.java
+++ src/GestionPersonas.java
@@ -2,5 +2,12 @@ public class GestionPersonas {
     public ArrayList<Usuario> capturarUsuario(Scanner scanner) {
         ArrayList<Usuario> usuarios = new ArrayList<>();
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese los datos de la persona " + (i + 1));
 
-    }
+            System.out.print("Nombre: ");
+            String nombre = scanner.nextLine();
+
+
+
+        }
