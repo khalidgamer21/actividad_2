94fdc618bbbdc46c89a39774fbbb9d41c0c4c5ac
marlon <marlon.monterrosa.m@gmail.com>
2025-05-21 20:15:34 -0500

    Se crea un objeto Scanner para leer entrada por teclado.
    Se crea un objeto GestionPersonas para usar sus métodos.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        GestionPersonas gestion = new GestionPersonas();
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
