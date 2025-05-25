e797302087a082f9b46732c2ea81e3120dc54cdc
marlon <marlon.monterrosa.m@gmail.com>
2025-05-25 09:51:06 -0500

    Este método sobrescribe (@Override) el método abstracto de Persona.Aquí se aplica el polimorfismo, porque cada tipo de persona puede mostrar su información de manera distinta.

--- src/Usuario.java
+++ src/Usuario.java
@@ -3,4 +3,12 @@ public class Usuario extends  Persona {
         super(nombre, apellido, genero, edad);
     }
 
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Apellido: " + getApellido());
+        System.out.println("Género: " + getGenero());
+        System.out.println("Edad: " + getEdad());
+    }
+
 }
